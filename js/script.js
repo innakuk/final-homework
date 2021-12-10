dbb4cf295f5119fe39361523409044c4ef9e9212
innakuk <kukharinna@gmail.com>
2021-12-10 12:50:00 +0200

    page about

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ $('.slider__list').slick({
     arrows: false,
 });
 
+$('.achievement-slider__list').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4
+    
+});
+
