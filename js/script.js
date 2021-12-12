bd9fe27b890bfa7e74ce7c9c720391ffbc4965c9
innakuk <kukharinna@gmail.com>
2021-12-12 19:32:10 +0200

    team entry page

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@ $('.slider__list').slick({
 
 $('.achievement-slider__list').slick({
     dots: true,
+    dotsClass: 'ach-slider__dots',
     arrows: false,
     infinite: true,
     slidesToShow: 4,
-    slidesToScroll: 4
-    
+    slidesToScroll: 4,
 });
 
